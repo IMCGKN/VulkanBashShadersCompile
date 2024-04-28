e6082065a625b4eba140277fa6ce1a33471f43b9
IMCGKN <113937505+IMCGKN@users.noreply.github.com>
2024-04-28 16:22:49 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,8 +4,3 @@ cd shaders/
 for fileName in *{.frag,.vert}; do
     glslc $fileName -o $fileName.spv
 done
-
-# glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
-# glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
-# glslc shaders/point_light.frag -o shaders/point_light.frag.spv
-# glslc shaders/point_light.frag -o shaders/point_light.frag.spv
