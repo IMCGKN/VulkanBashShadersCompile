6ef905746d31c92a699a87aa0a6f94ae6c57c2be
IMCGKN <113937505+IMCGKN@users.noreply.github.com>
2024-05-01 13:14:30 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+# !!!!!!!!!!!!!! WORKS ONLY IN LINUX   !!!!!!!!!!!!!!
 
-cd shaders/
-for fileName in *{.frag,.vert}; do
-    glslc $fileName -o $fileName.spv
-done
+if ! command -v glslc &> /dev/null; then
+    echo "<glslc> could not be found"
+    exit 1
+fi
+
+if [ -d shaders/ ]; then
+    cd shaders/
+    if [ -z "$(ls .)" ]; then
+        echo "No shaders!"
+    else
+        for fileName in *{.frag,.vert}; do
+            glslc $fileName -o $fileName.spv
+        done
+    fi
+else
+    echo "No folder named 'shaders'!"
+fi
